72a833893e41b44e8a5c4bbbf486d0c6375120b7
ykonomi <hgKaguyaToupako4er@gmail.com>
2019-01-08 13:37:00 +0900

    Refactoring

--- src/card.rs
+++ src/card.rs
@@ -70,39 +70,16 @@ impl Card {
     }
     pub fn is_available(&self, user: &User) -> bool {
         let jewelries = user.get_jewelries();
+        let colors = [Color::Black, Color::White, Color::Red, Color::Blue, Color::Green, Color::Gold];
 
-        let black_token = user.get_number_of_tokens(Color::Black);
-        let white_token = user.get_number_of_tokens(Color::White);
-        let red_token = user.get_number_of_tokens(Color::Red);
-        let blue_token = user.get_number_of_tokens(Color::Blue);
-        let green_token = user.get_number_of_tokens(Color::Green);
         let mut gold_token = user.get_number_of_tokens(Color::Gold) as i8;
 
-        self.estimate_gold_token(
-            black_token + jewelries.get_jewelry(Color::Black),
-            self.cost_black,
-            &mut gold_token,
-        );
-        self.estimate_gold_token(
-            white_token + jewelries.get_jewelry(Color::White),
-            self.cost_white,
-            &mut gold_token,
-        );
-        self.estimate_gold_token(
-            red_token + jewelries.get_jewelry(Color::Red),
-            self.cost_red,
-            &mut gold_token,
-        );
-        self.estimate_gold_token(
-            blue_token + jewelries.get_jewelry(Color::Blue),
-            self.cost_blue,
-            &mut gold_token,
-        );
-        self.estimate_gold_token(
-            green_token + jewelries.get_jewelry(Color::Green),
-            self.cost_green,
-            &mut gold_token,
-        );
+        for color in colors.iter() {
+            let token =  user.get_number_of_tokens(*color);
+            let jewelry = jewelries.get_jewelry(*color);
+            let cost = self.get_cost(*color);
+            self.estimate_gold_token(token + jewelry, cost, &mut gold_token);
+        }
 
         gold_token >= 0
     }
